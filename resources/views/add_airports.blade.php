f318c0f6c2411d06f60c7e9de0fa1508eaea7c67
PauliusKpl <theemzyhd@gmail.com>
2022-10-10 11:31:46 +0300

    prasau

--- resources/views/add_airports.blade.php
+++ resources/views/add_airports.blade.php
@@ -19,7 +19,33 @@
         <div class="d-grid gap-2 mx-auto mt-2">
             <label for="map" class="form-label">Map</label>
             <input class="form-control" type="text" style="background-color: rgb(231, 231, 231)" readonly>
-            <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d36698.85251817294!2d23.93765535!3d54.90856874999999!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e0!3m2!1sen!2slt!4v1663948509708!5m2!1sen!2slt" style="width:100%;height:500px; border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+            <script>
+                const locationElement = document.getElementById('location');
+                mapboxgl.accessToken = 'pk.eyJ1IjoidGl0YXNueGx0IiwiYSI6ImNqZWs3ZHliejBjOWMzM284aG1nbG1yN3IifQ._nFPiSI4HSaZriIEDwRa8g';
+                var map = new mapboxgl.Map({
+                    container: 'map',
+                    style: 'mapbox://styles/mapbox/streets-v11',
+                    doubleClickZoom: false
+                });
+
+                // Add the control to the map.
+                map.addControl(
+                    new MapboxGeocoder({
+                    accessToken: mapboxgl.accessToken,
+                    mapboxgl: mapboxgl
+                    })
+                );
+
+                let marker = new mapboxgl.Marker()
+                    .setLngLat([30.5, 50.5])
+                    .addTo(map);
+
+                map.on('dblclick', (e) => {
+                    marker.setLngLat(e.lngLat);
+                    locationElement.value = `${e.lngLat.lng} ${e.lngLat.lat}`;
+                })
+
+            </script>
         </div>
         <button type="submit" class="btn btn-success mt-3">Create</button>
     </form>
